925ee1a743412350b2b840c38df812e149127dd1
li3osoma <liza031008@mail.ru>
2023-04-21 12:45:22 +0300

    Threads and Handler are used

--- app/src/main/java/com/example/tryingroom/MainActivity.kt
+++ app/src/main/java/com/example/tryingroom/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.tryingroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.tryingroom.dao.NotionDao
 import com.example.tryingroom.database.NotionDatabase
 import com.example.tryingroom.databinding.ActivityMainBinding
 import com.example.tryingroom.entity.Notion
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,12 +20,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding= ActivityMainBinding.inflate(layoutInflater)
-
-        db=Room.databaseBuilder(applicationContext, NotionDatabase::class.java, "notions")
-            .allowMainThreadQueries()
-            .build()
+        db=Room.databaseBuilder(applicationContext, NotionDatabase::class.java, "notions").build()
         notionDao= (db as NotionDatabase).notionDao()
         updateUi()
+
+
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
@@ -33,20 +35,56 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun saveNotion(){
-        notionDao.insert(Notion(
-            binding.titleEditText.text.toString(),
-            binding.bodyEditText.text.toString()))
+        thread {
+            notionDao.insert(Notion(
+                binding.titleEditText.text.toString(),
+                binding.bodyEditText.text.toString()))
+        }.join()
     }
+
+
+//    class MyHandler: Handler() {
+//        override fun handleMessage(msg: Message) {
+//            super.handleMessage(msg)
+//            var bundle=msg.data
+//
+//            binding.titleText.text=bundle.getString("title")
+//            binding.bodyText.text=bundle.getString("text")
+//
+//        }
+//    }
 
     private fun updateUi(){
         var title=""
         var text=""
-        for(notion in notionDao.getNotionsList()){
-            title+=notion.title+"\n"
-            text+=notion.body+"\n"
+
+        val uiHandler=Handler(Looper.getMainLooper())
+
+        thread {
+            for(notion in notionDao.getNotionsList()){
+                title+=notion.title+"\n"
+                text+=notion.body+"\n"
+            }
+            uiHandler.post{
+                binding.titleText.text=title
+                binding.bodyText.text=text
+                binding.titleEditText.text.clear()
+                binding.bodyEditText.text.clear()
+            }
         }
-        binding.titleText.text=title
-        binding.bodyText.text=text
+
+//        Thread(
+//            Runnable {
+//                run(){
+//                    var bundle=Bundle()
+//                    for(notion in notionDao.getNotionsList()){
+//                        title+=notion.title+"\n"
+//                        text+=notion.body+"\n"
+//                    }
+//
+//                }
+//            }
+//        ).start()
     }
 
 
